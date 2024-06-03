a0884aebd3f8c20c69bf5bd51fee2fa098efd3b6
oaoba <hacchi0803.rera1126@gmail.com>
2024-06-03 23:21:38 +0900

    [finish] layout and routes

--- sample_app/test/integration/site_layout_test.rb
+++ sample_app/test/integration/site_layout_test.rb
@@ -5,9 +5,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
+    #一旦root_pathを３に設定（本来は２でpassする。未達成）
+    assert_select "a[href=?]", root_path, count: 3
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
