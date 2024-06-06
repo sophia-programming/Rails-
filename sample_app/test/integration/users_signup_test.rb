362469320dd18f87919f090b59c499a6558f6581
oaoba <hacchi0803.rera1126@gmail.com>
2024-06-06 16:53:00 +0900

    [finish] user signup

--- sample_app/test/integration/users_signup_test.rb
+++ sample_app/test/integration/users_signup_test.rb
@@ -15,4 +15,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation' # エラーメッセージが表示される
     assert_select 'div.field_with_errors' # エラーのフィールドが表示される
   end
+
+  test "valid signup information" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
 end
