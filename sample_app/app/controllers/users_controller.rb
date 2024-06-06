2dab7151f47644bf26968c522cb6097c30768a83
oaoba <hacchi0803.rera1126@gmail.com>
2024-06-06 16:21:37 +0900

    [add] ユーザー登録機能

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -4,5 +4,22 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)    # この行を追加
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
