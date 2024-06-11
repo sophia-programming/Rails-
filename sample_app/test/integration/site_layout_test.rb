8e392a36f9936e98d5b1b8740f022ff5d6b73c69
oaoba <hacchi0803.rera1126@gmail.com>
2024-06-11 17:26:19 +0900

    [add] ページネーション機能追加

--- sample_app/test/integration/site_layout_test.rb
+++ sample_app/test/integration/site_layout_test.rb
@@ -2,7 +2,12 @@ require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
+    # 未ログインユーザーの場合
     get root_path
     assert_template 'static_pages/home'
     #一旦root_pathを３に設定（本来は２でpassする。未達成）
@@ -12,5 +17,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     get signup_path
     # assert_select "title", full_title("Sign up")
+
+
+    # ログインユーザーの場合
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 3
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
