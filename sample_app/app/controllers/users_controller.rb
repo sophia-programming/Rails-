574584e7821b71d9df80db89d5265f478bc44bc6
oaoba <hacchi0803.rera1126@gmail.com>
2024-06-11 16:25:14 +0900

    [add] ユーザーにログインを要求する実装

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+   before_action :logged_in_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -8,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)    # この行を追加
+    @user = User.new(user_params)
     if @user.save
       reset_session
       log_in @user
@@ -23,9 +24,28 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+  # Before filters
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url, status: :see_other
+    end
+  end
 end
