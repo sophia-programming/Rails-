71fa9089b6969a4727f82f4dc08faa11975d85bf
oaoba <hacchi0803.rera1126@gmail.com>
2024-06-17 18:46:28 +0900

    [add] マイクロポストのサンプル

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
    end
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless @user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
