05b2214d20b8b0f60c6cf39ab9a2f5a669c114d4
oaoba <hacchi0803.rera1126@gmail.com>
2024-06-18 22:07:30 +0900

    [add] マイクロポストを削除する機能 (だがマイクロポストが表示されない...)

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -54,19 +54,19 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 
+  # beforeフィルター
 
-  # Before filters
-
-  # 正しいユーザーかどうか確認
+  # 正しいユーザーかどうかを確認
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url, status: :see_other) unless current_user?(@user)
   end
 
-  # 管理者かどうか確認
+  # 管理者かどうかを確認
   def admin_user
     redirect_to(root_url, status: :see_other) unless current_user.admin?
   end
